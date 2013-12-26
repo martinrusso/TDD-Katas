e131baad1b3c75396d3af63e9905bcdfe555ed0d
Martin Russo <russomartin88@gmail.com>
2013-12-26 18:10:12 +0100

    Allowing "Buy [number] get one free" type of offers

--- supermarket.rb
+++ supermarket.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
 
 class Supermarket
-  def self.price(catalog, cart) 
+  def self.price(catalog, cart, get_one_free_amount) 
 		total_price = 0
 		cart.each do |item, quantity| 
-			total_price += quantity*catalog[item]
+			item_price = catalog[item]
+			item_subtotal = quantity*item_price
+			if get_one_free_amount.include?item and quantity == get_one_free_amount[item]
+				item_subtotal -= item_price
+			end
+			total_price += item_subtotal 
 		end
 		total_price
   end
 end
+
+
--- tests.rb
+++ tests.rb
@@ -2,22 +2,32 @@ require "./supermarket"
 require "test/unit"
 
 class TC_Supermarket < Test::Unit::TestCase
+  def catalog
+   	{'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
+	end
 	def test_buy_one_product
-			catalog = {'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
 			cart = {'Loaf of Bread' => 1}
-			price = Supermarket.price(catalog, cart)
+			price = Supermarket.price(catalog, cart, {})
 			assert_equal(1, price)
 	end
 	def test_buy_one_of_each_product
-			catalog = {'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
 			cart = {'Loaf of Bread' => 1, 'Noodles' => 1, 'Soup cans' => 1}
-			price = Supermarket.price(catalog, cart)
+			price = Supermarket.price(catalog, cart, {})
 			assert_equal(3.5, price)
 	end
 	def test_buy_two_of_each_product
-			catalog = {'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
 			cart = {'Loaf of Bread' => 2, 'Noodles' => 2, 'Soup cans' => 2}
-			price = Supermarket.price(catalog, cart)
+			price = Supermarket.price(catalog, cart, {})
 			assert_equal(7, price)
+	end
+	def test_buy_two_noodles_get_one_free
+			cart = {'Noodles' => 3}
+			price = Supermarket.price(catalog, cart, {'Noodles' => 3})
+			assert_equal(1, price)
+	end
+	def test_buy_one_noodles_get_none_free
+			cart = {'Noodles' => 1}
+			price = Supermarket.price(catalog, cart, {'Noodles' => 3})
+			assert_equal(0.5, price)
 	end
 end
