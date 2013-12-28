1bdb2cf2566461a8eeab0f0d422c6be209331f71
Martin Russo <russomartin88@gmail.com>
2013-12-28 11:54:41 +0100

    Fixed indentation

--- supermarket.rb
+++ supermarket.rb
@@ -1,38 +1,38 @@
 #!/usr/bin/env ruby
-
+  
 class Supermarket
   def self.price(catalog, cart, one_for_free_required_quantities, offer) 
-		total = 0
-		cart.each do |item, quantity| 
-			price = catalog[item]
-			subtotal = calc_subtotal(quantity, price)
-			subtotal = apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
-			subtotal = apply_offer(subtotal, item, quantity, offer)
-			total += subtotal 
-		end
-		total
+    total = 0
+    cart.each do |item, quantity| 
+      price = catalog[item]
+      subtotal = calc_subtotal(quantity, price)
+      subtotal = apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
+      subtotal = apply_offer(subtotal, item, quantity, offer)
+      total += subtotal 
+    end
+    total
   end
 
-	def self.calc_subtotal(quantity, price)
-		quantity*price
+  def self.calc_subtotal(quantity, price)
+    quantity*price
   end
 
-	def self.apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
-			if one_for_free_required_quantities.include?item 
-						 if quantity == one_for_free_required_quantities[item]
-							 subtotal -= price 
-						 end
-			end
-			subtotal 
-	end
-	def self.apply_offer(subtotal, item, quantity, offer)
-		if offer[:product] == item
-			if offer[:quantity] == quantity
-				subtotal = offer[:price]
-			end
-		end		
-		return subtotal
-	end
+  def self.apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
+    if one_for_free_required_quantities.include?item 
+      if quantity == one_for_free_required_quantities[item]
+        subtotal -= price 
+      end
+    end
+    subtotal 
+  end
+  def self.apply_offer(subtotal, item, quantity, offer)
+    if offer[:product] == item
+      if offer[:quantity] == quantity
+        subtotal = offer[:price]
+      end
+    end   
+    return subtotal
+  end
 
 end
 
--- tests.rb
+++ tests.rb
@@ -2,40 +2,39 @@ require "./supermarket"
 require "test/unit"
 
 class TC_Supermarket < Test::Unit::TestCase
-	Offer = Struct.new(:product, :quantity, :price)
+  Offer = Struct.new(:product, :quantity, :price)
   def catalog
-   	{'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
-	end
-	def test_buy_one_product
-			cart = {'Loaf of Bread' => 1}
-			price = Supermarket.price(catalog, cart, {}, {})
-			assert_equal(1, price)
-	end
-	def test_buy_one_of_each_product
-			cart = {'Loaf of Bread' => 1, 'Noodles' => 1, 'Soup cans' => 1}
-			price = Supermarket.price(catalog, cart, {}, {})
-			assert_equal(3.5, price)
-	end
-	def test_buy_two_of_each_product
-			cart = {'Loaf of Bread' => 2, 'Noodles' => 2, 'Soup cans' => 2}
-			price = Supermarket.price(catalog, cart, {}, {})
-			assert_equal(7, price)
-	end
-	def test_buy_two_noodles_get_one_free
-			cart = {'Noodles' => 3}
-			price = Supermarket.price(catalog, cart, {'Noodles' => 3}, {})
-			assert_equal(1, price)
-	end
-	def test_buy_one_noodles_get_none_free
-			cart = {'Noodles' => 1}
-			price = Supermarket.price(catalog, cart, {'Noodles' => 3}, {})
-			assert_equal(0.5, price)
-	end
-	def test_buy_3_soup_cans_for_five
-			cart = {'Soup cans' => 3}
-			soup_cans_offer = Offer.new("Soup cans", 3, 5)
-			price = Supermarket.price(catalog, cart, {}, soup_cans_offer)
-			assert_equal(5, price)
-	end
-
+    {'Loaf of Bread' => 1, 'Noodles' => 0.5, 'Soup cans' => 2}
+  end
+  def test_buy_one_product
+    cart = {'Loaf of Bread' => 1}
+    price = Supermarket.price(catalog, cart, {}, {})
+    assert_equal(1, price)
+  end
+  def test_buy_one_of_each_product
+    cart = {'Loaf of Bread' => 1, 'Noodles' => 1, 'Soup cans' => 1}
+    price = Supermarket.price(catalog, cart, {}, {})
+    assert_equal(3.5, price)
+  end
+  def test_buy_two_of_each_product
+    cart = {'Loaf of Bread' => 2, 'Noodles' => 2, 'Soup cans' => 2}
+    price = Supermarket.price(catalog, cart, {}, {})
+    assert_equal(7, price)
+  end
+  def test_buy_two_noodles_get_one_free
+    cart = {'Noodles' => 3}
+    price = Supermarket.price(catalog, cart, {'Noodles' => 3}, {})
+    assert_equal(1, price)
+  end
+  def test_buy_one_noodles_get_none_free
+    cart = {'Noodles' => 1}
+    price = Supermarket.price(catalog, cart, {'Noodles' => 3}, {})
+    assert_equal(0.5, price)
+  end
+  def test_buy_3_soup_cans_for_five
+    cart = {'Soup cans' => 3}
+    soup_cans_offer = Offer.new("Soup cans", 3, 5)
+    price = Supermarket.price(catalog, cart, {}, soup_cans_offer)
+    assert_equal(5, price)
+  end
 end
