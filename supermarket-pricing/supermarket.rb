91555dad1f3ad4b67d1d5a04f2503ee85ba7c530
martinrusso <russomartin88@gmail.com>
2014-02-20 22:07:00 +0100

    Reduced number of arguments of help methods by using instance variables

--- supermarket-pricing/supermarket.rb
+++ supermarket-pricing/supermarket.rb
@@ -4,44 +4,41 @@ class Supermarket
   def self.price(catalog, cart, one_for_free_required_quantities, offers) 
     total = 0
     cart.each do |item, quantity| 
-      price = catalog[item]
-      subtotal = calc_subtotal(quantity, price)
-      subtotal = apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
-      subtotal = apply_offers(subtotal, item, price, quantity, offers)
-      total += subtotal 
+      @item = item
+      @price = catalog[@item]
+      @quantity = quantity
+      @subtotal = @quantity * @price
+      @subtotal = apply_one_free(one_for_free_required_quantities)
+      @subtotal = apply_offers(offers)
+      total += @subtotal
     end
     total
   end
-  def self.calc_subtotal(quantity, price)
-    quantity*price
-  end
-  def self.apply_one_free(subtotal, item, price, quantity, one_for_free_required_quantities)
-    amount = subtotal
-    if one_for_free_required_quantities.include?item 
-      remaining_quantity = quantity
-      required_quantity = one_for_free_required_quantities[item]
+  def self.apply_one_free(one_for_free_required_quantities)
+    amount = @subtotal
+    if one_for_free_required_quantities.include?@item
+      remaining_quantity = @quantity
+      required_quantity = one_for_free_required_quantities[@item]
       while remaining_quantity >= required_quantity
-        amount -= price 
+        amount -= @price
         remaining_quantity -= required_quantity
       end
     end
     amount
   end
-  def self.apply_offers(subtotal, item, price, quantity, offers)
-    amount = subtotal
+  def self.apply_offers(offers)
+    amount = @subtotal
     offers.each do |offer|
-      if offer[:product] == item
+      if offer[:product] == @item
         amount = 0
-        unbilled_quantity = quantity
+        unbilled_quantity = @quantity
         while unbilled_quantity >= offer[:quantity]
           amount += offer[:price]
           unbilled_quantity -= offer[:quantity]
         end
-        amount += unbilled_quantity*price
-      end   
+        amount += unbilled_quantity * @price
+      end
     end
     amount
   end
 end
-
-
