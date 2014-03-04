0f56e0e5b436c8c4fa0aa08b1e994d0204c9061b
martinrusso <russomartin88@gmail.com>
2014-03-05 00:09:21 +0100

    Renamed tests

--- word_chains/tests.py
+++ word_chains/tests.py
@@ -14,13 +14,13 @@ class TestWordChain(unittest.TestCase):
         result = word_chain.find_next_word("code")
         self.assertEqual(len(result), len("code"))
 
-    def test_is_valid_next_word(self):
+    def test_next_word_validator(self):
         result = word_chain.is_valid_next_word("code", "rods")
         self.assertFalse(result)
         result = word_chain.is_valid_next_word("code", "rode")
         self.assertTrue(result)
 
-    def test_returned_word_differs_by_one_letter(self):
+    def test_returned_word_is_valid(self):
         next_word = word_chain.find_next_word("code")
         result = word_chain.is_valid_next_word("code", next_word)
         self.assertTrue(result)
